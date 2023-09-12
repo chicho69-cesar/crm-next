b2f1deccb9e53434004854841dffab17231bea24
Cesar <cesarvillalobosolmos.01@gmail.com>
2023-09-11 21:51:04 -0600

    Refactor graphql server with typeDefs and resolvers

--- pages/api/graphql/index.ts
+++ pages/api/graphql/index.ts
@@ -1,18 +1,7 @@
 import { ApolloServer } from '@apollo/server'
 import { startServerAndCreateNextHandler } from '@as-integrations/next'
-import { gql } from 'graphql-tag'
 
-const typeDefs = gql`
-  type Query {
-    hello: String
-  }
-`
-
-const resolvers = {
-  Query: {
-    hello: () => 'Hello world!',
-  }
-}
+import { resolvers, typeDefs } from '@/graphql/server'
 
 const server = new ApolloServer({
   typeDefs,
